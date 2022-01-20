1ca646acd501652137ecfe491a16db0ffc32785f
Yarot Kell <mechild02@gmail.com>
2022-01-20 16:14:26 -0500

    [CMD] updated some 'fun' commands.

--- app/monitor/commands/fun.rs
+++ app/monitor/commands/fun.rs
@@ -1,3 +1,4 @@
+/// Commands used for general amusement.
 #[group]
 #[summary="General amusement."]
 #[commands(hug, slap)]
@@ -8,17 +9,28 @@ pub struct Fun;
 #[command]
 pub async fn hug(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult
 {
-   // Send a message in the channel where the command was received.
-   // This should never be an Err, so we can--in theory--safely discard
-   // the result of this function call.
-   let _ = msg.channel_id.send_message(&ctx.http, |m| {
-      m.embed(|e| {
-         e.title("Function: Hug");
-         e.description("This operation is not yet fully implemented.");
+   let mut rnd       = rngs::OsRng;
+   let num: usize    = rnd.gen_range(1..=25) as usize;
+   let gif: Vec<Gif> = (&GIPHY).search("hug").await.expect("couldn't retrieve GIF");
+   let par: String   = args.single().expect("invalid arguments");
 
-         e
-      })
-   }).await?;
+   if let Some(mem) = &msg.member {
+      if let Some(usr) = mem.user.clone() {
+         // Send a message in the channel where the command was received.
+         // This should never be an Err, so we can--in theory--safely discard
+         // the result of this function call.
+         let _ = msg.channel_id.send_message(&ctx.http, |m| {
+            m.embed(|e| {
+               e.description(format!("{} hugged {}!", usr.name, par));
+               e.image(&gif[num].url);
+
+               e
+            })
+         }).await?;
+      } else {
+         msg.reply(ctx, "Couldn't hug your special someone! Sorry!").await?;
+      }
+   }
 
    return Ok(());
 }
@@ -27,23 +39,40 @@ pub async fn hug(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult
 #[command]
 pub async fn slap(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult
 {
-   // Send a message in the channel where the command was received.
-   // This should never be an Err, so we can--in theory--safely discard
-   // the result of this function call.
-   let _ = msg.channel_id.send_message(&ctx.http, |m| {
-      m.embed(|e| {
-         e.title("Function: Slap");
-         e.description("This operation is not yet fully implemented.");
+   let mut rnd       = rngs::OsRng;
+   let num: usize    = rnd.gen_range(1..=25) as usize;
+   let gif: Vec<Gif> = (&GIPHY).search("slap").await.expect("couldn't retrieve GIF");
+   let par: String   = args.single().expect("invalid arguments");
 
-         e
-      })
-   }).await?;
+   if let Some(mem) = &msg.member {
+      if let Some(usr) = mem.user.clone() {
+         // Send a message in the channel where the command was received.
+         // This should never be an Err, so we can--in theory--safely discard
+         // the result of this function call.
+         let _ = msg.channel_id.send_message(&ctx.http, |m| {
+            m.embed(|e| {
+               e.description(format!("{} slapped {}!", usr.name, par));
+               e.image(&gif[num].url);
+
+               e
+            })
+         }).await?;
+      } else {
+         msg.reply(ctx, "Couldn't slap anyone.").await?;
+      }
+   }
 
    return Ok(());
 }
 
 
 // IMPORTS ////////////////////////////////////////////////////////////////////////////////////////
+
+use crate::GIPHY;
+
+use giphy::v1::Gif;
+
+use rand::{Rng, rngs};
 
 use serenity::{
    client::{
@@ -59,5 +88,6 @@ use serenity::{
    },
    model::{
       channel::Message,
+      user::User,
    },
 };
